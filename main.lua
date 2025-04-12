8f94b18afd2245f9bfeebf027eaef2e17697e00d
Dexter Delaski <git@dextros.net>
2025-04-12 01:35:52 -0700

    V0.1
    Added V1 Sprites, left and write movement, sprite wraps around screen, Added acess to 32 bit color pallete changed background color

--- main.lua
+++ main.lua
@@ -1,5 +1,28 @@
+ poke(0x5f2e,1)
+
 function _init()
-    cls(1)
-    print("hello hello♥");
-    printh("hello hello ♥");
-   end
+    positionx = 0
+    speed = 2
+    
+end
+
+function _update()
+    if btn(➡️) then
+        positionx += 1 * speed
+    end
+
+    if btn(⬅️) then
+        positionx -= 1 * speed
+    end
+    if positionx>128 then
+    positionx=-17
+    else if positionx<-16 then
+        positionx=128
+    end
+end
+end
+
+function _draw()
+    cls(28)
+    spr(2, positionx, 50,2,2)
+end
