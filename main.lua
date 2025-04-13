a89b76d33e396633f33a703f1cf5ab23f2c0ab90
Dexter Delaski <git@dextros.net>
2025-04-13 02:29:20 -0700

    wave improvements
    looks more natural now

--- main.lua
+++ main.lua
@@ -1,19 +1,13 @@
-
-
 function _init()
-    poke( 0x5f2e, 1 )
+    poke(0x5f2e, 1)
 
-
-_waterinit()
+    _waterinit()
 
     _initAnim()
-
 
     positionx = 0
     idle_s = .3
     speed = .8
-    
-    
 end
 
 function _update()
@@ -23,28 +17,31 @@ function _update()
 
     if btn(⬅️) then
         positionx -= 1 * speed
-    else positionx += 1* idle_s
+    else
+        positionx += 1 * idle_s
     end
 
-    if positionx>=128 then
-    positionx=-13
-    else if positionx<=-14 then
-        positionx=128
+    if positionx >= 128 then
+        positionx = -13
+    else
+        if positionx <= -14 then
+            positionx = 128
+        end
     end
-end
-_spriteAnim()
+    _spriteAnim()
 
-_waterupdate()
+    _waterupdate()
 end
-
 
 function _draw()
-    pal(11,12+128,1) --color 11 (is darker blue
-    pal(9,9+128,1)
-    pal(1,0,1) --color 1 is black
-    pal(3,1,1)
+    pal(11, 12 + 128, 1)
+    --color 11 (is darker blue
+    pal(9, 9 + 128, 1)
+    pal(1, 0, 1)
+    --color 1 is black
+    pal(3, 1, 1)
     cls(12)
     _waterdraw()
     -- rectfill(0,0,128,45,1)
-    spr(sprite_num, positionx, 50,2,2)
+    spr(sprite_num, positionx, 50, 2, 2)
 end
--- water.lua
+++ water.lua
@@ -1,24 +1,36 @@
-
 function _waterinit()
-wave_speed = 0 --time/animation counter
-wave_h = 40 --vertical starting pos
-amp = 1.9 --wave height
-freq = .02 --frequency of waves
+    wave_speed = 0
+    --time/animation counter
+    wave_h = 40
+    --vertical starting pos
+    amp = 1.4
+    --wave height
+    freq = .01
+    --frequency of waves
+    secondary_freq = .02
+    secondary_amp = 1.9
+    waveoffset = .1
 end
 
 function _waterupdate()
-wave_speed += 0.01 -- increment timer
+    wave_speed += 0.01
+    -- increment timer
 end
 
 function _waterdraw()
-    local wave_points = {} --store wave y-positions
+    local wave_points = {}
+    --store wave y-positions
 
-    for x=0,127 do
-        local y= wave_h + sin(x*freq + wave_speed)*amp
-        wave_points[x]= y
-        pset(x,y,11)
+    for x = 0, 127 do
+        main_wave = sin(x * freq + wave_speed) * amp
+        secondary_wave = sin(x * secondary_freq + wave_speed * waveoffset) * secondary_amp
+
+        local y = wave_h + main_wave + secondary_wave
+        wave_points[x] = y
+
+        pset(x, y - 2, 7)
     end
-    for x=0, 127 do 
-        rectfill(x, wave_points[x]+1,x,127,11)
+    for x = 0, 127 do
+        rectfill(x, wave_points[x] + 1, x, 127, 11)
     end
 end
